4b1e8b7e0c306811b4148d4dc66e3c2ad2299b3f
zheeno <efezinoukpowe@gmail.com>
2022-05-17 18:15:00 +0100

    mod

--- src/constants/enums.js
+++ src/constants/enums.js
@@ -1,5 +1,5 @@
 const Enums =  {
-    BASE_URL: 'https://z-vibes-engine.herokuapp.com/api/v1',
+    BASE_URL: 'https://z-vibes-live.herokuapp.com/api/v1',
     COLORS: {
         white: " #fff",
         orange: "#D3006C"
