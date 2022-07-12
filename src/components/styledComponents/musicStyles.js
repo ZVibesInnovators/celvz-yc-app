3c573079f96ea11b9a7cb1668d98d9b7f7271e1b
Ibukunoluwa Agboola <ibukunoluwa.agb@gmail.com>
2022-07-12 11:59:00 +0100

    Top tracks component

--- src/components/styledComponents/musicStyles.js
+++ src/components/styledComponents/musicStyles.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Bloodtype } from "@mui/icons-material";
 
 export const HeroWrapper = styled.div`
     
@@ -26,9 +34,34 @@ export const HeroWrapper = styled.div`
     }
 `
 
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+       color: theme.palette.common.white,
+    //   color: "#d81b60",
+      borderColor: "#393838",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+      borderColor: "#393838",
+    },
+  }));
+  
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
 export const TrackList = styled.div`
-    height: 50vh;
-    background-color: #111;
+    height: 60vh;
+    background: rgba(0, 0, 0,0.6);
     position: relative;
     
 
--- src/pages/Music/TopTracks.js
+++ src/pages/Music/TopTracks.js
@@ -1,7 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Table, TableBody, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
 import React from 'react';
 import { Col, Row } from "reactstrap";
-import { TrackList } from "../../components/styledComponents/musicStyles";
+import { TrackList, StyledTableCell, StyledTableRow } from "../../components/styledComponents/musicStyles";
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Chosen', "Marizu", 500, "04.28", "..."),
+    createData('Grateful', "CELVZ Loveworld Singers", 200, "04.28", "..."),
+    createData('Unending Praise', "CELVZ Loveworld Singers", 700, "04.28", "..."),
+    createData('DYP Theme Song', "CELVZ Loveworld Singers", "10,000", "04.28", "..."),
+    createData('Morning Sun', "Marizu", 7000, "04.28", "..."),
+];
 
 const RecommendedMusic = () => {
 
@@ -11,7 +23,32 @@ const RecommendedMusic = () => {
                 <Row className="mask">
 
                     <h3>Top Tracks</h3>
-                    
+                    <TableContainer component={Box} style={{marginTop: "20px"}}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell>SONG</StyledTableCell>
+                                    <StyledTableCell align="right">ARTIST</StyledTableCell>
+                                    <StyledTableCell align="right">DAILY PLAYS</StyledTableCell>
+                                    <StyledTableCell align="right">TIME</StyledTableCell>
+                                    <StyledTableCell align="right">OPTIONS</StyledTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <StyledTableRow key={row.name}>
+                                        <StyledTableCell component="th" scope="row">
+                                            {row.name}
+                                        </StyledTableCell>
+                                        <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                                        <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                                        <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                                        <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                                    </StyledTableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Row>
             </TrackList>
         </Box>
